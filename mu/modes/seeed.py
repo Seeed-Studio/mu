9ce492232db28cbb3c224196751ec41ab7a107fb
gobuyun <874751353@qq.com>
2019-10-12 15:15:57 +0800

    Modify code specification issues where some lines of code in seeed.py exceed 79

--- mu/modes/seeed.py
+++ mu/modes/seeed.py
@@ -495,7 +495,7 @@ class Downloader(QObject):
     finished = pyqtSignal(bool)
 
     def __init__(self, des_path, source_path, 
-                    reqTimeout=5, readTimeout=0, try_time=3):
+            reqTimeout=5, readTimeout=0, try_time=3):
         super(Downloader, self).__init__()
         self.retStatus = False
         self.source_path = source_path
@@ -528,8 +528,8 @@ class Downloader(QObject):
         print("[DEBUG]------Request")
         try:
             if self.try_time:
-                self.reply = self.networkManager.get(\
-                            QNetworkRequest(QUrl(self.source_path)))
+                self.reply = self.networkManager.get(
+                    QNetworkRequest(QUrl(self.source_path)))
                 self.reply.finished.connect(self.onFinished)
                 self.reply.readyRead.connect(self.onReadyRead)
                 self.try_time = self.try_time - 1
@@ -567,7 +567,7 @@ class Downloader(QObject):
             # write
             with open(self.des_path, 'w') as fp:
                 print("write file:%s" % fp.write(str(self.data, 
-                                                encoding='utf-8')))
+                    encoding='utf-8')))
 
             print("finish download %s" % self.des_path)
             self.retStatus = True
@@ -670,8 +670,8 @@ class FirmwareUpdater(QThread):
 
     def check_new_lib(self):
         print('check lib')
-        if not self.confirmDownload(self.info.libaray_info_path, \
-                            self.info.cloud_libaray_info_path):
+        if not self.confirmDownload(self.info.libaray_info_path, 
+            self.info.cloud_libaray_info_path):
             print("not check and download new lib!")
             return
 
@@ -938,12 +938,12 @@ class SeeedMode(MicroPythonMode):
             if mode.file_extensions:
                 extensions += mode.file_extensions
         extensions = set([e.lower() for e in extensions])
-        extensions = '*.{} *.{}'.format(' *.'.join(extensions), \
-                                        ' *.'.join(extensions).upper())
+        extensions = '*.{} *.{}'.format(' *.'.join(extensions), 
+            ' *.'.join(extensions).upper())
         folder = super().workspace_dir()
         allow_previous = False
-        path = self.view.get_load_path(folder, extensions, \
-                                        allow_previous=allow_previous)
+        path = self.view.get_load_path(folder, extensions, 
+            allow_previous=allow_previous)
         if path:
             self.current_path = os.path.dirname(os.path.abspath(path))
             self.editor._load(path)
